852c0fe60e6788b84cf3e297eb2195eba61c4a70
Ivan <mayhemk57@gmail.com>
2025-03-21 15:18:48 -0600

    get account form set

--- src/components/AccountForm.vue
+++ src/components/AccountForm.vue
@@ -26,6 +26,38 @@ async function updateAccount() {
 
 
 <template>
+  <div>
+    <h3>Change Account details here!</h3>
+    <form @submit.prevent="updateAccount()">
+      <div>
+        <label for="accountName">Account Name</label>
+        <input v-model="editableAccountData.name" type="text" name="name" id="accountName" required maxlength="60">
+      </div>
+      <div>
+        <label for="accountBio">Account Bio</label>
+        <textarea v-model="editableAccountData.bio" name="bio" id="accountBio" maxlength="500"></textarea>
+      </div>
+      <div>
+        <label for="accountPicture">Account Picture</label>
+        <input v-model="editableAccountData.picture" type="url" id="accountPicture" name="picture" maxlength="500">
+      </div>
+      <div>
+        <label for="accountCoverImg">Account Cover Image</label>
+        <input v-model="editableAccountData.coverImg" type="url" name="coverImg" id="accountCoverImg" maxlength="500">
+      </div>
+      <div>
+        <label for="accountResume">Account Resume</label>
+        <input v-model="editableAccountData.resume" type="url" id="accountResume" name="resume" maxlength="500">
+      </div>
+      <div>
+        <label for="accountGraduated">Graduated?</label>
+        <input v-model="editableAccountData.graduated" type="checkbox" id="accountGraduated" name="graduated">
+      </div>
+      <div class="text-end">
+        <button class="btn btn-success" type="submit">submit</button>
+      </div>
+    </form>
+  </div>
 
 </template>
 
